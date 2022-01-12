a9a4515ec9b31ec68a62fd22448e940f55b58adf
Захар Молчанов <zvmolchanov@edu.hse.ru>
2022-01-12 18:03:54 +0500

    Добавьте файлы проекта.

---
+++ Work with gitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work_with_gitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
